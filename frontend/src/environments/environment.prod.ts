f4d5c82e82b31d3a1a1d48a858f9914e7ab45410
unknown <lovelove0969935644@gmail.com>
2026-09-19 23:43:24 +0700

    Configure Google OAuth client ID

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://computer-gaming-api.onrender.com/api',
   apiHost: 'https://computer-gaming-api.onrender.com',
-  googleClientId: '',
+  googleClientId: '464487179650-ge6q3ueatpj7rfhf8q0s9o001p8p4csg.apps.googleusercontent.com',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   apiHost: 'http://localhost:3000',
-  googleClientId: '',
+  googleClientId: '464487179650-ge6q3ueatpj7rfhf8q0s9o001p8p4csg.apps.googleusercontent.com',
 };
